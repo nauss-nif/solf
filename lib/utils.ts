1276c5e06500fe5b042d2075c8d2d90d131dff5a
nif305 <NIF305@gmail.com>
2026-04-17 18:25:38 +0300

    Fix tailwind merge helper

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,33 @@
-import { clsx, type ClassValue } from 'tailwind-merge'
+import { twMerge } from 'tailwind-merge'
+
+type ClassValue =
+  | string
+  | number
+  | null
+  | undefined
+  | false
+  | Record<string, boolean>
+  | ClassValue[]
+
+function toClassName(input: ClassValue): string {
+  if (!input) return ''
+
+  if (typeof input === 'string' || typeof input === 'number') {
+    return String(input)
+  }
+
+  if (Array.isArray(input)) {
+    return input.map(toClassName).filter(Boolean).join(' ')
+  }
+
+  return Object.entries(input)
+    .filter(([, value]) => value)
+    .map(([key]) => key)
+    .join(' ')
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs)
+  return twMerge(inputs.map(toClassName).filter(Boolean).join(' '))
 }
 
 export function numberToArabicWords(number: number): string {
